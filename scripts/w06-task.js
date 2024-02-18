d18223204b142ae3b7137465048d4c2566e827a0
thiagovieira89 <vieira.thiagoc@gmail.com>
2024-02-18 00:00:14 -0300

    close to finish

--- scripts/w06-task.js
+++ scripts/w06-task.js
@@ -1,26 +1,17 @@
-statesList = [];
-const userChoice = document.querySelector('#state');
-
-
-document.getElementById('state').addEventListener('change',displayStates);
+let statesList = {};
+const panelStates = document.getElementById('panel');
 
 //reset the user choice and showing the another choice.
 const reset = function(){
+    
 };
 
 //this functions get the value that the user choose, and call the API with the state selected
 //by the user.
 function displayStates() {
-
-    reset();
     const stateSelected = document.getElementById('state').value;
-    console.log (stateSelected);
-
-    if (stateSelected !== displayStates.previousState){
-        displayStates.previousState = stateSelected;
-    }
-
-    const apiUrl = `https://covid19-brazil-api.vercel.app/api/report/v1/brazil/uf/${stateSelected.toUpperCase()}`;
+    const apiUrl = `https://covid19-brazil-api.vercel.app/api/report/v1/brazil/uf/${stateSelected.toUppercase()}`;
+    
     fetch(apiUrl)
         .then(response => {
             if (response.ok) {
@@ -32,41 +23,38 @@ function displayStates() {
         })
         .then(data => {
             // use the datas as you need to show on the screem!
-            let cases = data.cases;
-            let death = data.deaths;
-            let suspected = data.suspects;
-
+            statesList[stateSelected]={
+                cases: data.cases,
+                deaths: data.deaths,
+                suspects: data.suspects
+        };
+            
+        updatePanel(stateSelected);
             console.log('Cases Numbers:', cases);
-            console.log('Death Numbers:', death);
-            console.log('Suspects Numbers:', suspected);    
+            console.log('Death Numbers:', deaths);
+            console.log('Suspects Numbers:', suspects);    
         })
         .catch(error => {
             // handler error
-            console.log('ERRO NA CHAMADA', error);
+            console.log('ERROR CALLING', error);
         });
-
 }
 
-const displayStates = (statesList) =>{
-    statesList.forEach(state => {
-        
-        
-    });
+function updatePanel(state) {
+    let cases = statesList[state].cases;
+    let deaths = statesList[state].deaths;
+    let suspects = statesList[state].suspects;
+
+    document.getElementById('cases').textContent = ` Cases Numbers: ${cases}`;
+    document.getElementById('deaths').textContent = ` Death Numbers: ${deaths}`;
+    document.getElementById('suspects').textContent = ` Suspects Numbers: ${suspects}`;
 }
 
-const getStates = async () => {
-    const response = await fetch('https://covid19-brazil-api.vercel.app/api/report/v1/brazil/uf');
+document.getElementById('state').addEventListener('change',function (){
+    reset()
+    displayStates();
+});
 
-    if (response.ok) {
-
-        const data = await response.json();
-        statesList = data;
-        displayStates(statesList);
-        console.log(statesList)
-    }
-};
-
-
-getStates();
+displayStates();
 
 
